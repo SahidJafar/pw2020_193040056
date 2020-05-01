a0e9c06ad98b6e08d13f8f45e19cf6f0e9e1d523
SahidJafar <sahid11.sj@gmail.com>
2020-05-01 17:51:00 +0700

    pw2020_193040056 praktikum P6_PW_193040056

--- praktikum/P6_PW_193040056/latihan6b/index.php
+++ praktikum/P6_PW_193040056/latihan6b/index.php
@@ -12,8 +12,10 @@ $laptop = query("SELECT * FROM laptop")
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
   <!-- Jquery -->
   <script type="text/javascript" src="js/jquery.js"></script>
@@ -21,61 +23,90 @@ $laptop = query("SELECT * FROM laptop")
 
   <title>index193040056</title>
 
+  <!-- MYCSS -->
+  <style>
+
+  </style>
 </head>
 
-<body style="background-color: black;">
+<body>
   <!-- NAVBAR -->
-  <nav class="navbar  fixed-top navbar-expand-sm navbar-dark bg-dark">
+  <nav class="navbar  fixed-top navbar-expand-sm navbar-light bg-white">
     <div class="container-fluid">
       <div class="navbar-header">
         <a class="navbar-brand" href="#">
-          <img src="assets/img/ASUS.png" width="120" height="80" class="d-inline-block align-top" alt="">
+          <img src="assets/img/logo.png" width="150" height="100" class="d-inline-block align-top" alt="">
         </a>
       </div>
-      <h4 style="color: black">IN SEARCH OF INCREDIBLE</h4>
     </div>
   </nav>
 
-  <!-- Jumbotron ROG -->
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="jumbotron jumbotron-fluid " style="background-color: black; margin-top: 100px;">
-          <div class="container text-center">
-            <img src="assets/img/ROG.png" alt="" class="display-4">
-            <h1 class="lead" style="margin-top: 20px; font-size: 50px; color: red;">REPUBLIC OF GAMERS</h1>
-          </div>
-        </div>
+
+  <!-- Slider -->
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active mb-5">
+        <img src="assets/img/slide1.jpg" height="550px" width="auto" class="d-block w-100" alt="...">
       </div>
-      <div>
+      <div class="carousel-item">
+        <img src="assets/img/slide3.jpg" height="550px" width="auto" class="d-block w-100 image2" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="assets/img/slider2.jpg" height="550px" width="auto" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 
-        <section class="laptop">
-          <div class="container" id="container">
-            <div class="row pt-5">
 
-              <?php foreach ($laptop as $l) : ?>
+  <section class="laptop">
+    <div class="container" id="container">
+      <div class="row pt-5">
 
-                <div class="col-sm-3 mb-3">
-                  <div class="card  mt-3" style="background-color: black;">
-                    <img class="card-img-top" src="assets/img/<?= $l['Foto'] ?>" alt="Card image cap" height="300" width="150">
-                    <div class="card-body" style="background-color: black;">
-                      <h5 class="card-title"><?= $l["Nama"] ?></h5>
-                      <a href="php/detail.php?Id=<?= $l['Id'] ?>" class="btn btn-primary">Detail</a>
-                    </div>
-                  </div>
-                </div>
+        <?php foreach ($laptop as $l) : ?>
 
-              <?php endforeach ?>
-
+          <div class="col-sm-3 mb-3">
+            <div class="card  mt-3">
+              <img class="card-img-top" src="assets/img/<?= $l['Foto'] ?>" alt="Card image cap" height="200" width="150">
+              <div class="card-body">
+                <h5 class="card-title"><?= $l["Nama"] ?></h5>
+                <a href="php/detail.php?Id=<?= $l['Id'] ?>" class="btn btn-danger">Detail</a>
+              </div>
             </div>
           </div>
-        </section>
 
-        <!-- Optional JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <?php endforeach ?>
+
+      </div>
+    </div>
+  </section>
+
+
+
+
+  <!-- Optional JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script>
+    $('.carousel').carousel({
+      interval: 2000
+    })
+  </script>
 </body>
 
 </html
