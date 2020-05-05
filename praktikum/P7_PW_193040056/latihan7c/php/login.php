4635e78a82050634877c575c0cc5e277b6d52d46
SahidJafar <sahid11.sj@gmail.com>
2020-05-05 20:36:53 +0700

    pw2020_193040056 praktikum P7_PW_193040056

--- praktikum/P7_PW_193040056/latihan7c/php/login.php
+++ praktikum/P7_PW_193040056/latihan7c/php/login.php
@@ -55,51 +55,102 @@ if (isset($_POST['login'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!-- MYCSS -->
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background-color: #E31C11;
+  }
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  .box {
+    width: 500px;
+    padding: 40px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    text-align: center;
+  }
+
+  .box h1 {
+    color: black;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+
+  .box input[type="text"],
+  .box input[type="password"] {
+    border: 0;
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: 2px solid #db001a;
+    padding: 14px 10px;
+    width: 200px;
+    outline: none;
+    color: black;
+    border-radius: 24px;
+    transform: 0, 25s;
+
+  }
+
+  .box input[type="text"]:focus,
+  .box input[type="password"]:focus {
+    width: 280px;
+    border-color: #8200b5;
+  }
+
+  .box input[type="submit"] {
+    border: 0;
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: 2px solid #8200b5;
+    padding: 14px 40px;
+    outline: none;
+    color: red;
+    border-radius: 24px;
+    transition: 0.25s;
+    cursor: pointer;
+  }
+
+  .box input[type="submit"]:hover {
+    background: #8200b5;
+  }
+</style>
 </head>
 
 <body>
-  <h3>Form Login</h3>
 
-  <form action="" method="POST">
+
+  <form class="box" action="" method="POST">
+    <h1>Login</h1>
     <?php if (isset($error)) : ?>
       <p style="color: red; font-style: italic;">Username atau Password Salah</p>
 
     <?php endif; ?>
-    <ul>
-      <li>
-        <label>
-          Username :
-          <input type="text" name="username" autofocus autocomplete="off" required>
-        </label>
-      </li>
-      <li>
-        <label>
-          Password :
-          <input type="password" name="password" required>
-        </label>
-      <li>
-        <input type="checkbox" name="remember">
-        <label for="remember">Remember me</label>
-      </li>
-      <li>
-        <button type="submit" name="login">Login</button>
-      </li>
+    <input type="text" name="username" placeholder="Username" autofocus autocomplete="off" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <input type="checkbox" name="remember">
+    <label for="remember">Remember me</label>
+    <p>Belum punya akun? Registrasi <a href="registrasi.php">Disini</a></p>
+    <input type="submit" name="login" value="Login">
 
-      <div class="registrasi">
-        <p>Belum punya akun? Registrasi <a href="registrasi.php">Disini</a></p>
-      </div>
-    </ul>
 
 
 
   </form>
+
+  <!-- Optional JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 
 </html>
