f271d50a273a2bf35e1a73378894e450df950d30
SahidJafar <sahid11.sj@gmail.com>
2020-05-20 21:05:36 +0700

    Finalproject_193040056

--- tubes/tubes_193040056.sql
+++ tubes/tubes_193040056.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2020 at 02:37 PM
+-- Generation Time: May 20, 2020 at 04:03 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -42,16 +42,16 @@ CREATE TABLE `laptop` (
 --
 
 INSERT INTO `laptop` (`Id`, `Foto`, `Nama`, `Processor`, `Keterangan`, `Harga`) VALUES
-(1, '1.PNG\r\n', 'Asus Laptop 15 A509f', 'Intel Core i5-8265u', 'RAM 4GB DDR4,1TB HDD,Graphic NVIDIA Geforce MX 230 2GB VRAM', 'Rp.8.199.000,00'),
-(2, 'z.PNG', 'Asus Vivobook A407', 'Intel Core i3-6006U', 'RAM 4GB DDR4,1TB HDD,Intel HD Graphics 520', 'Rp.5.699.000,00'),
+(1, '1.png\r\n', 'Asus Laptop 15 A509f', 'Intel Core i5-8265u', 'RAM 4GB DDR4,1TB HDD,Graphic NVIDIA Geforce MX 230 2GB VRAM', 'Rp.8.199.000,00'),
+(2, 'z.png', 'Asus Vivobook A407', 'Intel Core i3-6006U', 'RAM 4GB DDR4,1TB HDD,Intel HD Graphics 520', 'Rp.5.699.000,00'),
 (3, '3.png', 'Asus X409UA', 'Intel Core i3 7020U', 'RAM 4GB DDR4,1TB HDD,512GB SSD,Intel UHD Graphics 620', 'Rp.6.499.000,00'),
 (4, '4.png\r\n', 'Asus Vivobook X403FA', 'Intel Core i5-8265U', 'RAM 8GB,512GB SSD,Intel UHD Graphics 620', 'Rp.9.799.000,00'),
-(5, '5.PNG', 'Asus Tuf Gaming FX50', 'AMD Ryzen 5-3550H Processor 2.1GHz', '8GB RAM DDR4,1TB HDD,AMD Radeon RX560X 4GB GDDR5 VRAM', 'Rp.11.299.000,00'),
+(5, '5.png', 'Asus Tuf Gaming FX50', 'AMD Ryzen 5-3550H Processor 2.1GHz', '8GB RAM DDR4,1TB HDD,AMD Radeon RX560X 4GB GDDR5 VRAM', 'Rp.11.299.000,00'),
 (6, 'x.png\r\n\r\n', 'Asus ROG Strix G', 'Intel Core i7-9750H Hexa Core', '8 GB RAM DDR4,256 GB M.2 NVMe PCIe SSD + 1 TB SSHD,Nvidia GeForce RTX 2070 8 GB GDDR6 VRAM', 'Rp 18.299.000,00'),
 (7, '7.png', 'Asus Vivobook S14', 'Intel Core i5', '8 GB RAM DDR4,HDD 1TB SSD 128 GB,Nvidia GeForce MX150 Graphic 4GB VRam', 'Rp.11.499.000,00'),
-(8, '8.PNG', 'Asus Zenbook 13 UX33', 'Intel Core i5-8265U', '8 GB RAM DDR4, 256 SDD PCIe® 3.0 x2 SSD,Intel UHD Graphics 630', 'Rp.12.999.000,00'),
-(9, '9.PNG', 'Asus Zenbook Flip 14', 'AMD Ryzen 5 3500U', '8 GB RAM DDR4, 512 GB SSD, AMD Radeon Vega 10 Graphics', 'Rp 10.699.000,00'),
-(10, '10.PNG', 'Asus ROG Zephyrus S ', 'Intel Core i7-8750H', '8GB RAM DDR4, 1TB SSD, NVIDIA® GeForce RTX™ 2080 with Max-Q Design 8GB GDDR6 VRAM', 'Rp 47.999.000,00');
+(8, '8.png', 'Asus Zenbook 13 UX33', 'Intel Core i5-8265U', '8 GB RAM DDR4, 256 SDD PCIe® 3.0 x2 SSD,Intel UHD Graphics 630', 'Rp.12.999.000,00'),
+(9, '9.png', 'Asus Zenbook Flip 14', 'AMD Ryzen 5 3500U', '8 GB RAM DDR4, 512 GB SSD, AMD Radeon Vega 10 Graphics', 'Rp 10.699.000,00'),
+(10, '10.png', 'Asus ROG Zephyrus S ', 'Intel Core i7-8750H', '8GB RAM DDR4, 1TB SSD, NVIDIA® GeForce RTX™ 2080 with Max-Q Design 8GB GDDR6 VRAM', 'Rp 47.999.000,00');
 
 -- --------------------------------------------------------
 
